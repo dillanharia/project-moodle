1ab53fda0e707b35b8bfa47e3db533d2f8212a57
Dillan Haria <dillan.haria@gmail.com>
2026-03-23 16:26:20 +0000

    Added initial dinosaur card dataset with rarity tiers and pricing for use in gameplay

--- src/projectmoodle/CardLibrary.java
+++ src/projectmoodle/CardLibrary.java
@@ -8,7 +8,33 @@ public class CardLibrary {
 	
 	public CardLibrary() {
 		allCards = new ArrayList<>();
+		initialiseCards();
 	}
+	
+	private void initialiseCards() {
+		
+		// COMMON
+		allCards.add(new Card(101, "Compsognathus", Rarity.COMMON, 10));
+		allCards.add(new Card(102, "Hypsilophodon", Rarity.COMMON, 12));
+		allCards.add(new Card(103, "Gallimimus", Rarity.COMMON, 15));
+		
+		// UNCOMMON
+		allCards.add(new Card(201, "Parasaurolophus", Rarity.COMMON, 25));
+		allCards.add(new Card(202, "Ankylosaurus", Rarity.UNCOMMON, 30));
+	    allCards.add(new Card(203, "Iguanodon", Rarity.UNCOMMON, 28));
+
+	    // RARE
+	    allCards.add(new Card(301, "Stegosaurus", Rarity.RARE, 45));
+	    allCards.add(new Card(302, "Allosaurus", Rarity.RARE, 50));
+	    allCards.add(new Card(303, "Carnotaurus", Rarity.RARE, 55));
+
+	     // UNIQUE
+	    allCards.add(new Card(401, "Tyrannosaurus Rex", Rarity.UNIQUE, 100));
+	    allCards.add(new Card(402, "Spinosaurus", Rarity.UNIQUE, 110));
+	    allCards.add(new Card(403, "Giganotosaurus", Rarity.UNIQUE, 120));
+	}
+	
+	
 	
 	public ArrayList<Card> getAllCards() {
 		return allCards;
--- src/projectmoodle/Game.java
+++ src/projectmoodle/Game.java
@@ -3,17 +3,12 @@ package projectmoodle;
 public class Game {
 	
 	public void start() {
-		Player player = new Player("dillan", 100);
-		
-        player.addCard(new Card(101, "Bug Byte", Rarity.COMMON, 15));
-        player.addCard(new Card(205, "Null Phantom", Rarity.RARE, 40));
-        player.addCard(new Card(150, "Alpha Script", Rarity.COMMON, 20));
+		CardLibrary library = new CardLibrary();
 		
 		
-		System.out.println("Welcome to Project Moodle.");
-        System.out.println("Player: " + player.getUsername());
-        System.out.println("Credits: " + player.getCredits());
-        player.displayOwnedCards();
-
+		System.out.println("All cards in the game:");
+        for (Card card : library.getAllCards()) {
+        	System.out.println(card);
+        }
 	}
 }
