c5dffe44a1039d1e972c7202bd796ac226b6b06c
Dillan Haria <dillan.haria@gmail.com>
2026-03-23 16:21:23 +0000

    Added a CardLibrary class to manage the collection of all available cards in the game

--- src/projectmoodle/CardLibrary.java
+++ src/projectmoodle/CardLibrary.java
@@ -1,5 +1,17 @@
 package projectmoodle;
 
+import java.util.ArrayList;
+
 public class CardLibrary {
 
+	private ArrayList<Card> allCards;
+	
+	public CardLibrary() {
+		allCards = new ArrayList<>();
+	}
+	
+	public ArrayList<Card> getAllCards() {
+		return allCards;
+	}
+	
 }
