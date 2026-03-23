2b526ef88693b793c3e1c5cfbf4910ff543893ea
Dillan Haria <dillan.haria@gmail.com>
2026-03-23 16:45:09 +0000

    implemented User Login via terminal input and authentication check using LoginService

--- src/projectmoodle/Game.java
+++ src/projectmoodle/Game.java
@@ -1,12 +1,40 @@
 package projectmoodle;
 
+import java.util.Scanner;
+
 public class Game {
 	
+	private Scanner scanner;
+	private LoginService loginService;
+	
+	public Game() {
+		scanner = new Scanner(System.in);
+		loginService = new LoginService();;
+	}
+	
+	
 	public void start() {
-        LoginService loginService = new LoginService();
+        System.out.println("Welcome to Project Moodle");
+        System.out.println("Please log in to continue.");
+        System.out.println();
         
-        System.out.println(loginService.authenticate("dillanharia", "dino123"));
-        System.out.println(loginService.authenticate("wrongUser", "wrongPass"));
+        System.out.print("Username: ");
+        String username = scanner.nextLine();
+        
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+        
+        boolean loginSuccessful = loginService.authenticate(username,  password);
+        
+        if (!loginSuccessful) {
+        	System.out.println();
+        	System.out.println("Login failed. Exciting game.");
+        	return;
+        }
+        
+        System.out.println();
+        System.out.println("Login successful");
+        System.out.println("Welcome, " + username + "!");
         
 	}
 }
