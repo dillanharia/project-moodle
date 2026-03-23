c61acf34583c928f40bb0453ea3ac6055df0c07f
Dillan Haria <dillan.haria@gmail.com>
2026-03-23 16:15:20 +0000

    Added the player class to store the user's credits, owned cards with helper methods for adding, removing, finding, sorting and displaying cards

--- src/projectmoodle/Game.java
+++ src/projectmoodle/Game.java
@@ -3,10 +3,17 @@ package projectmoodle;
 public class Game {
 	
 	public void start() {
-		System.out.println("Welcome to Project Moodle.");
-		System.out.println("Game setup is in progress... ");
+		Player player = new Player("dillan", 100);
 		
-		Card card = new Card(101, "Boss Bandit", Rarity.COMMON, 10);
-		System.out.println(card);
+        player.addCard(new Card(101, "Bug Byte", Rarity.COMMON, 15));
+        player.addCard(new Card(205, "Null Phantom", Rarity.RARE, 40));
+        player.addCard(new Card(150, "Alpha Script", Rarity.COMMON, 20));
+		
+		
+		System.out.println("Welcome to Project Moodle.");
+        System.out.println("Player: " + player.getUsername());
+        System.out.println("Credits: " + player.getCredits());
+        player.displayOwnedCards();
+
 	}
 }
--- src/projectmoodle/Player.java
+++ src/projectmoodle/Player.java
@@ -1,4 +1,84 @@
 package projectmoodle;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 public class Player {
+	private String username;
+	private int credits;
+	private ArrayList<Card> ownedCards;
+	
+	public Player(String username, int startingCredits) {
+		this.username = username;
+		this.credits = startingCredits;
+		this.ownedCards = new ArrayList<>();
+	}
+	
+	
+	public String getUsername() {
+	return username;
+	}
+	
+	public int getCredits() {
+	return credits;
+	}
+	
+	public ArrayList<Card> getOwnedCards() {
+		return ownedCards;
+	}
+	
+	public void addCard(Card card) {
+		ownedCards.add(card);
+	}
+	
+	public boolean removeCardById(int cardId) {
+		for (int i = 0; i < ownedCards.size(); i++) {
+			if (ownedCards.get(i).getId() == cardId) {
+				ownedCards.remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Card getCardById(int cardId) {
+		for (Card card : ownedCards) {
+			if (card.getId() == cardId) {
+				return card;
+			}
+		}
+		return null;
+	}
+	
+	public void sortOwnedCards() {
+		Collections.sort(ownedCards);
+	}
+	
+	public void addCredits(int amount) {
+	credits += amount;
+	}
+	
+	public boolean spendCredits(int amount) {
+		if (amount > credits) {
+			return false;
+		}
+		credits -= amount;
+		return true;
+	}
+	
+	public void displayOwnedCards() {
+		sortOwnedCards();
+		
+		if (ownedCards.isEmpty()) {
+			System.out.println("You do not own any cards yet.");
+			return;
+		}
+		
+		System.out.println("Your cards:");
+		for (Card card : ownedCards) {
+			System.out.println(card);
+		}
+	}
+	
 }
