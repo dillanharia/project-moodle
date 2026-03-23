b9fef677b691bf6df3b365d321df4be179bc1c37
Dillan Haria <dillan.haria@gmail.com>
2026-03-23 16:35:30 +0000

    Added a simple local login service with hardcoded fake credentials just to support the games authentication flow

--- src/projectmoodle/Game.java
+++ src/projectmoodle/Game.java
@@ -3,11 +3,10 @@ package projectmoodle;
 public class Game {
 	
 	public void start() {
-        CardLibrary library = new CardLibrary();
-
-        System.out.println("Welcome to Project Moodle");
-        System.out.println("Your random test card is:");
-        System.out.println(library.getRandomCard());
+        LoginService loginService = new LoginService();
+        
+        System.out.println(loginService.authenticate("dillanharia", "dino123"));
+        System.out.println(loginService.authenticate("wrongUser", "wrongPass"));
         
 	}
 }
--- src/projectmoodle/LoginService.java
+++ src/projectmoodle/LoginService.java
@@ -2,4 +2,17 @@ package projectmoodle;
 
 public class LoginService {
 
+	private final String validUsername;
+	private final String validPassword;
+	
+	public LoginService() {
+		validUsername = "dillanharia";
+		validPassword = "dino123";
+	}
+	
+	public boolean authenticate (String username, String password) {
+		return username.equals(validUsername) && password.equals(validPassword);
+	}
+	
+	
 }
